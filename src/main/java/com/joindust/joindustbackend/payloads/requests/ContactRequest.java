528428f8a92a152b713c95f58bcefb169057883b
Leonardo Murça <leonardoamurca@gmail.com>
2019-12-03 11:47:15 -0300

    feat: add contacts payloads

---
+++ src/main/java/com/joindust/joindustbackend/payloads/requests/ContactRequest.java
@@ -0,0 +1,14 @@
+package com.joindust.joindustbackend.payloads.requests;
+
+public class ContactRequest {
+  private Long userId;
+
+  public Long getUserId() {
+    return userId;
+  }
+
+  public void setUserId(Long userId) {
+    this.userId = userId;
+  }
+
+}
---
+++ src/main/java/com/joindust/joindustbackend/payloads/responses/UserContacts.java
@@ -0,0 +1,1 @@
+package com.joindust.joindustbackend.payloads.responses;
