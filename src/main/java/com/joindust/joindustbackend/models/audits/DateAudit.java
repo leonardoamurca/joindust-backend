d6f074c22fee182d3cb79ec77b865e17cb310405
Leonardo Murça <leonardoamurca@gmail.com>
2019-12-15 11:59:30 -0300

    chore: migrating audits to lombok notation

--- src/main/java/com/joindust/joindustbackend/models/audits/DateAudit.java
+++ src/main/java/com/joindust/joindustbackend/models/audits/DateAudit.java
@@ -12,9 +12,14 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = { "createdAt", "updatedAt" }, allowGetters = true)
+@Getter
+@Setter
 public abstract class DateAudit implements Serializable {
 
   private static final long serialVersionUID = -2660419203246659193L;
@@ -26,21 +31,5 @@ public abstract class DateAudit implements Serializable {
   @LastModifiedDate
   @Column(nullable = false)
   private Instant updatedAt;
-
-  public Instant getCreatedAt() {
-    return createdAt;
-  }
-
-  public void setCreatedAt(Instant createdAt) {
-    this.createdAt = createdAt;
-  }
-
-  public Instant getUpdatedAt() {
-    return updatedAt;
-  }
-
-  public void setUpdatedAt(Instant updatedAt) {
-    this.updatedAt = updatedAt;
-  }
 
 }
