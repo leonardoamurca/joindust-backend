e49e846b714ac33f9c1b192ebe49dfc4d782d878
Leonardo Murça <leonardoamurca@gmail.com>
2020-01-13 11:35:30 -0300

    feat: adding log AOP

--- src/main/java/com/joindust/joindustbackend/Application.java
+++ src/main/java/com/joindust/joindustbackend/Application.java
@@ -7,6 +7,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EntityScan (basePackageClasses = {Application.class, Jsr310JpaConverters.class})
 @EnableSwagger2
+@EnableAspectJAutoProxy
 public class Application {
 
   @PostConstruct
---
+++ src/main/java/com/joindust/joindustbackend/aspects/LoggingAspect.java
@@ -0,0 +1,39 @@
+package com.joindust.joindustbackend.aspects;
+
+import lombok.SneakyThrows;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+  private static final Logger LOGGER = LogManager.getLogger(LoggingAspect.class);
+
+  @SneakyThrows
+  @Around ("execution(* com.joindust.joindustbackend.controllers..*(..)))")
+  public Object profileAllMethods(ProceedingJoinPoint proceedingJoinPoint) {
+    MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+    String className = methodSignature.getDeclaringTypeName();
+    String methodName = methodSignature.getName();
+
+    final StopWatch stopWatch = new StopWatch();
+
+    stopWatch.start();
+    Object result = proceedingJoinPoint.proceed();
+    stopWatch.stop();
+
+    LOGGER.info("Execution time of " + className + "." + methodName + " " + ":: " + stopWatch.getTotalTimeMillis() + " ms");
+
+    return result;
+
+  }
+}
