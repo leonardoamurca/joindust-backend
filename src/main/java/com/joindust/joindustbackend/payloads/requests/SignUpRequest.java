d5fa0ca904655f75a8bfcc90fb05ebab325b6694
Leonardo Murça <leonardoamurca@gmail.com>
2020-01-20 22:58:04 -0300

    chore: add custom validation messages for SignupRequest and LoginRequest

--- src/main/java/com/joindust/joindustbackend/payloads/requests/SignUpRequest.java
+++ src/main/java/com/joindust/joindustbackend/payloads/requests/SignUpRequest.java
@@ -11,28 +11,27 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SignUpRequest {
-  @NotBlank
-  @Size(min = 4, max = 40)
+  @Size (min = 4, max = 40, message = "O nome da empresa deve conter no mínimo {min} e no máximo {max} caracteres")
   private String corporateName;
 
-  @NotBlank
-  @Size(min = 3, max = 15)
+  @NotBlank (message = "O nome de usuário não pode ser deixado em branco.")
+  @Size (min = 3, max = 15, message = "O nome de usuário deve conter no mínimo {min} e no máximo {max} caracteres.")
   private String username;
 
-  @NotBlank
-  @Size(max = 40)
-  @Email
+  @NotBlank (message = "O e-mail não pode ser deixado em branco.")
+  @Size (max = 40, message = "Seu e-mail deve conter no máximo {max} caracteres.")
+  @Email (message = "O e-mail inserido não é válido.")
   private String email;
 
-  @NotBlank
-  @Size(min = 6, max = 20)
+  @NotBlank (message = "Sua senha não pode ser deixada em branco.")
+  @Size (min = 6, max = 20, message = "Sua senha deve ter no mínimo {min} e no máximo {max} dígitos.")
   private String password;
 
-  @NotNull
-  @Size(max = 14, min = 14)
+  @NotNull (message = "O cnpj não pode ser deixado em branco.")
+  @Size (max = 14, min = 14, message = "O cnpj deve ter exatamente {max} dígitos.")
   private String cnpj;
 
-  @Size(min = 9, max = 9)
+  @Size (min = 9, max = 9, message = "O telefone deve ter exatamente {max} dígitos.")
   private String phone;
 
   private Integer roleId;
